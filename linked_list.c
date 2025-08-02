16e09de56f6bccd2e4b077d044a4c21389afac77
Foyez Ahmed <ahmedfoyez2004@gmail.com>
2025-08-02 06:01:57 +0600

    update linked-list

--- linked_list.c
+++ linked_list.c
@@ -7,24 +7,23 @@ struct node{
 };
 struct node* head=NULL;
 struct node* tail=NULL;
-
+void push_back(int a){
+    struct node* newnode=malloc(sizeof(struct node));
+    newnode->x=a;
+    newnode->next=NULL;
+    newnode->prev=tail;
+    if (tail==NULL) head=newnode;
+    else tail->next=newnode;
+    tail=newnode;
+}
 void push_front(int a){
     struct node* newnode=malloc(sizeof(struct node));
     newnode->x=a;
     newnode->prev=NULL;
     newnode->next=head;
-    if (head==NULL) tail=newnode;
+    if (head==NULL) tail=newnode; 
     else head->prev=newnode;
     head=newnode;
-}
-void push_back(int a){
-    struct node* newnode=malloc(sizeof(struct node));
-    newnode->x=a;
-    newnode->prev=tail;
-    newnode->next=NULL;
-    if (tail==NULL) head=newnode;
-    else tail->next=newnode;
-    tail=newnode;
 }
 void pop_front(){
     if (head==NULL) return;
@@ -42,6 +41,33 @@ void pop_back(){
     if (tail==NULL) head=NULL;
     else tail->next=NULL;
 }
+void pop_node(int g){
+    if (head==NULL) return;
+    struct node* temp=head;
+    while (temp!=NULL){
+        if (temp->x==g){
+            if (temp==head){
+                pop_front();
+                break;
+            } 
+            else if (temp==tail) {
+                pop_back();
+                break;
+            } 
+            else{
+                struct node* temp_prev=temp;
+                struct node* temp_next=temp;
+                temp_prev=temp_prev->prev;
+                temp_next=temp_next->next;
+                free(temp);
+                temp_prev->next=temp_next;
+                temp_next->prev=temp_prev;
+                break;
+            }  
+        }
+        temp=temp->next;
+    }
+}
 int main(){
     push_front(5);
     push_front(3);
@@ -50,6 +76,8 @@ int main(){
     push_back(23);
     pop_back();
     pop_front();
+    pop_node(5);
+    pop_node(30);
     struct node* temp = head;
     while (temp != NULL) {
         printf("%d ",temp->x);
---
+++ linkedlistsingly.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(){
+    push_back(5);
+    push_back(4);
+    push_back(3);
+    push_back(2);
+    delete();
+
+}
